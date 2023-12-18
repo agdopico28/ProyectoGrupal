bd78285a2186bb6d85c592c69b141505e3a4a8bd
alu10701951 <alugar1951@ieselcaminas.org>
2023-12-18 15:15:15 +0100

    Initial commit

---
+++ src/main/java/com/example/proyectogrupal/ProyectoGrupalApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectogrupal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoGrupalApplication.class, args);
+    }
+
+}
