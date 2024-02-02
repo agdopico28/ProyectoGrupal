37dba006de788599b349295d4f9ad0e6ccd60330
Jose Luis Muriach <alumur497@ieselcaminas.org>
2024-02-02 15:42:18 +0100

    Testing class "MainTest"

--- src/main/java/com/example/proyectogrupal/MainTest.java
+++ src/main/java/com/example/proyectogrupal/MainTest.java
@@ -1,12 +1,25 @@
 package com.example.proyectogrupal;
 
-import com.example.proyectogrupal.entity.Clase;
+import com.example.proyectogrupal.entity.*;
+
+import java.util.Date;
 
 public class MainTest {
     public static void main(String[] args) {
-        /*Clase nuevaClase = new Clase(1L, "Juan", "segundo", "mstricula",
-                "Acceso a datos", "rojo");*/
 
-        //Hay que probarlo
+        final UsuarioAdmin joseluis = new UsuarioAdmin(1, "Rayden", "Jose Luis Muriach",
+                "joseluis@jose.com", "test1234");
+        final UsuarioAdmin amalia = new UsuarioAdmin(2, "agdopico", "Amalia Garcia",
+                "amalia@amalia.com", "test1234");
+        final UsuarioAdmin alejandro = new UsuarioAdmin(3, "Destroyer", "Alejandro Corro",
+                "alejandro@alex.com", "test1234");
+
+        Profesor pedro = new Profesor("1", "Pedro", "PeSeg", 111111111,
+                "11111111A", "pedro@pedro.com");
+        Curso segundoDAM = new Curso(1L, "Acceso a datos", "Curso de acceso a los datos",
+                new Date(01,10,2019), new Date(01,01,2020), true);
+        Matricula matriculacion = new Matricula(1L, joseluis, segundoDAM, true);
+        Clase nuevaClase = new Clase(1L, pedro, segundoDAM, matriculacion,
+                "Acceso a datos", "rojo");
     }
 }
--- src/main/java/com/example/proyectogrupal/repositories/ScheduleRepContract.java
+++ src/main/java/com/example/proyectogrupal/repositories/ScheduleRepContract.java
@@ -5,9 +5,7 @@ import com.example.proyectogrupal.entity.Horarios;
 public interface ScheduleRepContract {
     void findById(Integer id);
     void save(Horarios horarios);
-
     void update(Horarios horarios);
-
     void delete(Integer id);
 
 }
