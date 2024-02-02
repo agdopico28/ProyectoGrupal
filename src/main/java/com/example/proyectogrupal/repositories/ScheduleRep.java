7ed5863e017e73063ead489770ddb0896b290af2
amali <alugar1951@ieselcaminas.oreg>
2024-02-02 15:45:24 +0100

    fin schedule

--- src/main/java/com/example/proyectogrupal/repositories/ScheduleRep.java
+++ src/main/java/com/example/proyectogrupal/repositories/ScheduleRep.java
@@ -4,12 +4,23 @@ import com.example.proyectogrupal.entity.Horarios;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class ScheduleRep implements ScheduleRepContract{
     private EntityManager entityManager;
 
+    private final Map<Long,Horarios> horarios = new HashMap<>();
     public ScheduleRep(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Horarios> allSchedule() {
+        return new ArrayList<>(horarios.values());
     }
 
     @Override
